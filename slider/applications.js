60e8effc669413bccefad190ab7fd665a80b4171
Michal Vanko <michalvankosk@gmail.com>
2013-04-09 13:16:29 +0200

    xml with jquery

--- slider/applications.js
+++ slider/applications.js
@@ -1,19 +1,20 @@
 // AJAX call for matches for slider
 var xmlhttp = new XMLHttpRequest();
 var eventXml;
+var slider = $('#myslider');
 xmlhttp.onreadystatechange = function()
 {
 	//We will use this condition if we had files running on web server
 	// instead we'll use condition for local access
 	// to run it you'll need to use --allow-file-access-from-files flag ! (chrome)
 	//if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	if (xmlhttp.response != "")
+	if (xmlhttp.response != "" && xmlhttp.responseXML != null)
 	{
 
 		eventXml = xmlhttp.responseXML;
-		console.log(eventXml);
+		jxml = $('event', eventXml);
+		
 	}
 }
 xmlhttp.open("GET","sliderdata.xml",true);
 xmlhttp.send();
-eventXml.getElementsByTagName('events')[0].getElementsByTagName('event')[0].getAttribute("startDate");
