a2f00e3f92dee134fc99dbbd81b4dae51e208975
Michal Vanko <michalvankosk@gmail.com>
2013-04-09 11:53:44 +0200

    Loaded local XML file

--- slider/applications.js
+++ slider/applications.js
@@ -1,12 +1,19 @@
 // AJAX call for matches for slider
 var xmlhttp = new XMLHttpRequest();
+var eventXml;
 xmlhttp.onreadystatechange = function()
 {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	//We will use this condition if we had files running on web server
+	// instead we'll use condition for local access
+	// to run it you'll need to use --allow-file-access-from-files flag ! (chrome)
+	//if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	if (xmlhttp.response != "")
 	{
-		var eventXml = xmlhttp.responseXML;
+
+		eventXml = xmlhttp.responseXML;
 		console.log(eventXml);
 	}
 }
-xmlhttp.open("GET","http://www.livesport.tv/streaming/eventList/0,,14976,00.html?partnerId=4044&days=10&showCompletedEvents=false",true);
+xmlhttp.open("GET","sliderdata.xml",true);
 xmlhttp.send();
+eventXml.getElementsByTagName('events')[0].getElementsByTagName('event')[0].getAttribute("startDate");
