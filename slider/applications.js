b7897e46722c62ac549b4d381256afc758fbd8d7
Michal Vanko <michalvankosk@gmail.com>
2013-04-09 13:54:54 +0200

    Functional slider with no styles nor countdown

--- slider/applications.js
+++ slider/applications.js
@@ -1,20 +1,45 @@
-// AJAX call for matches for slider
-var xmlhttp = new XMLHttpRequest();
-var eventXml;
-var slider = $('#myslider');
-xmlhttp.onreadystatechange = function()
+$(document).ready(function()
 {
-	//We will use this condition if we had files running on web server
-	// instead we'll use condition for local access
-	// to run it you'll need to use --allow-file-access-from-files flag ! (chrome)
-	//if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	if (xmlhttp.response != "" && xmlhttp.responseXML != null)
-	{
+	// AJAX call for matches for slider
+	var xmlhttp = new XMLHttpRequest();
+	var eventXml;
+	//Define slider
+	var slider = $('#myslider');
+	// Slider options
+	var sliderOptions = {
+		speed: 300,
+		slideMargin: 100,
+		auto: true,
+		autoHover: true,
+		pause: 5000,
+		pagerType: 'short',
+		pagerSelector: '#sliderpager',
+		nextSelector: '#slidernext',
+		prevSelector: '#sliderprev',
 
-		eventXml = xmlhttp.responseXML;
-		jxml = $('event', eventXml);
-		
+	};
+
+	xmlhttp.onreadystatechange = function()
+	{
+		//We will use this condition if we had files running on web server
+		// instead we'll use condition for local access
+		// to run it you'll need to use --allow-file-access-from-files flag ! (chrome)
+		//if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		if (xmlhttp.response != "" && xmlhttp.responseXML != null)
+		{
+
+			eventXml = xmlhttp.responseXML;
+			jxml = $('event', eventXml);
+			
+			for (var i = 0; i < 5 && i < jxml.length; i++)
+			{
+				slider.html(slider.html() + "<li><div style='height: 50px;'>" + jxml.eq(i).attr('startDate') + "</div></li>");
+				
+			}
+			$('.bxslider').bxSlider(sliderOptions);
+		}
 	}
-}
-xmlhttp.open("GET","sliderdata.xml",true);
-xmlhttp.send();
+	xmlhttp.open("GET","sliderdata.xml",true);
+	xmlhttp.send();
+
+});
