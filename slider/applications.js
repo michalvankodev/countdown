12e3dbc033e4389b8bcaff970c13b07b4090cf12
Michal Vanko <michalvankosk@gmail.com>
2013-04-10 09:49:42 +0200

    made countdowns and slides

--- slider/applications.js
+++ slider/applications.js
@@ -27,14 +27,98 @@ $(document).ready(function()
 		//if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		if (xmlhttp.response != "" && xmlhttp.responseXML != null)
 		{
-
+			// get XML
 			eventXml = xmlhttp.responseXML;
 			jxml = $('event', eventXml);
 			
+			// For first five elements make countdowns
 			for (var i = 0; i < 5 && i < jxml.length; i++)
 			{
-				slider.html(slider.html() + "<li><div style='height: 50px;'>" + jxml.eq(i).attr('startDate') + "</div></li>");
+				// divide text into separate strings
+				var matchText = jxml.eq(i).text().trim().split('<BR>');
 				
+				// remove 'SOCCER:' from string
+				var soccerpos = matchText[0].indexOf('SOCCER:');
+				if ( soccerpos > -1)
+				{
+					matchText[0] = matchText[0].substr(soccerpos+7).trim();
+				}
+
+				//parse date
+				var startDate = new Date(jxml.eq(i).attr('start'));
+				var dateString = startDate.toLocaleString();
+				var jsonDate = startDate.toJSON();
+
+
+				// list item to add
+				var $listItem = $('<li></li>');
+				// header with title of the match
+				var $header = $('<h1></h1>', 
+				{
+					text: matchText[0],
+				});
+
+				//additional info
+				var $info = $('<span></span>');
+				if (matchText[1])
+				{
+					console.log('existuje');
+					$info.text(matchText[1]);
+				}
+				else 
+				{
+					// make info empty element
+					$info = $([]);
+				}
+
+				$time = $('<time></time>',
+				{
+					'datetime': jsonDate,
+					text: dateString,
+				});
+				// create countdown div element and start countdown
+				var $countdown = $('<div></div',
+				{	
+					'id': 'count'+i,
+					'class': 'countdown',
+				});
+
+				// Text to display after countdown ends
+				var $liveText = $('<div></div>',
+				{
+					text: 'LIVE ! <span class="info"> Watch now</span>',
+					'class': 'countdownlive',
+				});
+
+				// set options for countdown
+				var cOptions =
+				{
+					dString: 'd',
+				    hString: 'h',
+				    mString: 'm',
+				    sString: 's',
+		            dStringSingle: 'd',
+			        hStringSingle: 'h',
+			        mStringSingle: 'm',
+			        sStringSingle: 's',
+			        dateTo: startDate,
+
+			        // SET element to display after countdown ends
+			        onCountDownEnd: function()
+				    {
+				    	$countdown.hide();
+				    	$liveText.show();
+				    }
+				}
+				// Start countdown
+				$countdown.countdown('init', cOptions);
+
+				// append elements to list
+				$listItem.append($header, $info, $time, $countdown, $liveText);
+				
+				// add new match into slider
+				slider.append($listItem);
+
 			}
 			$('.bxslider').bxSlider(sliderOptions);
 		}
