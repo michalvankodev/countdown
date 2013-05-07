1fe55859d99c7aab1c39bb9de8712769d3d9960f
Michal Vanko <mklakakilli@gmail.com>
2013-05-07 12:35:55 +0200

    very very very goood xslt filtration... ! :) im awesome

--- results/applications.js
+++ results/applications.js
@@ -1,11 +1,15 @@
 $(document).ready(function()
 {
+	var myMatches;
+	var seasonsXml;
+	var matchesXml
+	var matchesXsl;
 	$.ajax({
 	url: 'seasons.xml',
 	dataType: "xml"
 	}).done(function(data)
 	{
-		var seasonsXml = data;
+		 seasonsXml = data;
 		$.ajax({
 		url: 'seasonsfilter.xsl',
 		dataType: "xml"
@@ -14,8 +18,8 @@ $(document).ready(function()
 			var seasonsXsl = xsldata;
 			var xsltProcessor=new XSLTProcessor();
 			xsltProcessor.importStylesheet(seasonsXsl);
-			var resultDocument = xsltProcessor.transformToFragment(seasonsXml, document);
-			$('#leagues').html(resultDocument);
+			var mySeason = xsltProcessor.transformToFragment(seasonsXml, document);
+			$('#leagues').html(mySeason);
 			$('#leagues').change();
 			
 		}).fail(function()
@@ -49,28 +53,36 @@ $(document).ready(function()
 		dataType: 'xml'
 		}).done(function(seasonData)
 		{
-			var matchesXml = seasonData;
+			matchesXml = seasonData;
 			console.log(matchesXml);
 			$.ajax({
 			url: 'matches.xsl',
 			dataType: 'xml'
 			}).done (function(matchesData)
 			{
-				var matchesXsl = $.parseXML(matchesData);
-				console.log(matchesXsl);
-				console.log(matchesXml);
+				matchesXsl = matchesData;
 				var matchProc = new XSLTProcessor();
 				matchProc.importStylesheet(matchesXsl);
-				resultDocument = matchProc.transformToFragment(matchesXml, document);
-				console.log(resultDocument);
-				lolo =  $('team', $(resultDocument).children());
-				
-				lolo.each(function()
-					{
-						console.log($(this).text());
-					});
-				
-				
+				myMatches = matchProc.transformToFragment(matchesXml, document);
+				var teams =  $('teams > team', $(myMatches).children());
+				var teamsToAppend = '<option value="all"> all </option>';
+
+				teams.each(function()
+				{
+					teamsToAppend += '<option value="' + $(this).text() + '"> '+ $(this).text() + '</option>';
+				});
+				$('#team').html(teamsToAppend);
+
+				var dates = $('dates > date', $(myMatches).children());
+
+				var datesToAppend = '<option value="all"> Select date </option>';
+				dates.each(function ()
+				{
+					datesToAppend += '<option value="' + $(this).text() + '"> '+ $(this).text() + '</option>';
+
+				});
+				$('#date').html(datesToAppend);
+				$('#date').change();
 			});
 
 			
@@ -85,4 +97,54 @@ $(document).ready(function()
 			$('#results').append(error);
 		});
 	});
+	$('#team').change(function ()
+	{
+		showMatches();
+	});
+	$('#date').change(function ()
+	{
+		showMatches();
+	});
+
+	function showMatches()
+	{
+
+		$('#results > table').remove();
+		var filterTeam = '';
+		var filterDate = '';
+		var selectedTeam = $('#team').val();
+		var selectedDate = $('#date').val();
+
+		if (selectedTeam != '' && selectedTeam != 'all')
+		{
+			filterTeam = "[@team_A_name = '"+ selectedTeam +"' or @team_B_name = '"+ selectedTeam +"']";
+		}
+
+		if (selectedDate != '' && selectedDate != 'all')
+		{
+			filterDate = "[@date_utc = '"+ selectedDate +"']";
+		}
+
+		var select = "//match[@status='Played']"+filterTeam+filterDate+"/@date_utc[generate-id() = generate-id(key('ddates',.)[1])]"
+
+		console.log(matchesXsl);
+		$('variable[name="mojselect"]', matchesXsl).attr('select', select);
+
+		console.log(matchesXml);
+		newXml = matchesXml;
+		console.l
+		console.log(matchesXsl);
+		var x = new XSLTProcessor();
+		x.importStylesheet(matchesXsl);
+		displayMatches = x.transformToFragment(newXml, document);
+
+		 
+		console.log(displayMatches);
+		var tables = $('matches table', $(displayMatches).children());
+
+		console.log(tables);
+		$('#results').append(tables);
+		
+
+	}
 });
--- results/matches.xsl
+++ results/matches.xsl
@@ -1,29 +1,69 @@
 <?xml version="1.0" encoding="utf-8"?>
-<xsl:stylesheet version="2.0"
-xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-xmlns:fn="http://www.w3.org/2005/xpath-functions"
->
-
-<xsl:template match="/">
-<div>
-    <xsl:for-each select="//match" name="match">
-		<xsl:if test="contains(attribute::status, 'Played')">
-			<match>
-				<teamA><xsl:value-of select="attribute::team_A_name" /></teamA>
-				<teamB><xsl:value-of select="attribute::team_B_name"/></teamB>
-				<date><xsl:value-of select="attribute::date_utc"/></date>
-				<fsA><xsl:value-of select="attribute::fs_A"/></fsA>
-				<fsB><xsl:value-of select="attribute::fs_B"/></fsB>
-			</match>
-		</xsl:if> 
-    </xsl:for-each>
-	<xsl:for-each select="//match/@team_A_name" name="teams">
-		<xsl:sort select="."/>
-		<team>
-			<xsl:value-of select="." />
-		</team>
-    </xsl:for-each>
-</div>
-</xsl:template>
-
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	>
+	<xsl:key name="dteams" match="//match/@team_A_name" use="." />
+	<xsl:key name="ddates" match="//match/@date_utc" use="." />
+	<xsl:variable
+		name="mojselect"
+		select="//match[@status='Played']/@date_utc[generate-id() = generate-id(key('ddates',.)[1])]"/>
+		
+	<xsl:template match="/">
+		<season>
+			<xsl:attribute name="name">
+				<xsl:value-of select="//competition/@name" />
+			</xsl:attribute>
+			
+			<!-- GET TABLES OF MATCHES IN ONE DAY -->
+			<matches>
+				<xsl:for-each select="$mojselect">
+					<xsl:sort select="." order="descending"/>
+					<table>
+						<tr>
+							<th colspan="3">
+								<xsl:value-of select="//competition/@name"/>
+							</th>
+							<th colspan="2">
+								<xsl:value-of select="."/>
+							</th>
+						</tr>
+					<xsl:apply-templates select="../." />
+					</table>
+				</xsl:for-each>
+			</matches>
+			
+			
+			
+			
+			<teams>
+				<!-- DISTINCT VALUES OF TEAMS -->
+				<xsl:for-each select="//match/@team_A_name[generate-id() = generate-id(key('dteams',.)[1])]">
+					<xsl:sort select="."/>
+					<team>
+						<xsl:value-of select="." />
+					</team>
+				</xsl:for-each>
+			</teams>
+			<dates>
+				<!-- DISTINCT VALUES OF DATES -->
+				<xsl:for-each select="//match[@status='Played']/@date_utc[generate-id() = generate-id(key('ddates',.)[1])]">
+					<xsl:sort select="." order="descending"/>
+					<date>
+						<xsl:value-of select="." />
+					</date>
+				</xsl:for-each>
+			</dates>
+  
+		</season>
+	</xsl:template>
+	<xsl:template match="match">
+		<tr>
+			<td class="homeTeam"><xsl:value-of select="@team_A_name" /></td>
+			<td class="homeScore"><xsl:value-of select="@fs_A" /></td>
+			<td class="vs"> : </td>
+			<td class="awayScore"><xsl:value-of select="@fs_B" /></td>
+			<td class="awayTeam"><xsl:value-of select="@team_B_name" /></td>
+		</tr>
+	</xsl:template>    
+	
 </xsl:stylesheet>
